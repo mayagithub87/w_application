8b93b34aef4fe1267f4b8f4ccc29f44b900f2700
Mayte <maytemozo87@gmail.com>
2023-10-04 19:32:14 -0500

    cloud config server module

---
+++ msvc-cloud-config-server/src/test/java/cu/maya/springcloud/msvc/msvccloudconfigserver/MsvcCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cu.maya.springcloud.msvc.msvccloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
